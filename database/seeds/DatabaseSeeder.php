971c7f8ebea7228d97126e30a7a343ee24b406ed
Timothy Lutt <thejjokerr@gmail.com>
2017-03-24 15:57:59 +0100

    Seeds and PostController
    For testing api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(HousesTableSeeder::class);
+        $this->call(UsersAndHouseRolesTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
     }
 }
