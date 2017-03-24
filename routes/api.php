4bd39111e1c881fbaedfc5bf4a62082e9f3d6123
Radiuscollege <feddevangils@gmail.com>
2017-03-24 16:13:58 +0100

    no auth group

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth:api'], function(){
+Route::group([], function(){
     Route::resource('post', 'PostController', [
         'names' => [
             'index' => 'post.index',
