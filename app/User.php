cc77b628e900fc5dede34c77d78f62a0c27dfbca
Timothy Lutt <thejjokerr@gmail.com>
2017-03-30 16:49:07 +0200

    Fixed relation error

--- app/User.php
+++ app/User.php
@@ -27,4 +27,12 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * Get the house role associated with the model.
+     */
+    public function houseRole()
+    {
+        return $this->hasOne('App\HouseRole', 'user_id');
+    }
+
 }
