8427c4eb9a701788268e9c3b6a924c2e3e62aa6f
Radiuscollege <feddevangils@gmail.com>
2017-04-04 11:49:23 +0200

    add answer functionailty

---
+++ resources/views/partials/minis/_answer-form.blade.php
@@ -0,0 +1,9 @@
+<h2 class="is-2">Your answer</h2>
+<form action="{{action()}}">
+    <div class="field">
+        <p class="control">
+            <textarea class="textarea tinymce" name="content"></textarea>
+        </p>
+    </div>
+    <button class="button is-primary" type="submit">Submit</button>
+</form>
