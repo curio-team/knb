57af4f305fe08a577194adf67c981451108d1abb
Sven Luijten <svenluijten1995@gmail.com>
2017-04-06 12:43:48 +0200

    Clean up views

--- resources/views/partials/_posts.blade.php
+++ resources/views/partials/_posts.blade.php
@@ -10,23 +10,23 @@
                             <img src="http://bulma.io/images/placeholders/128x128.png">
                         </p>
                         <div class="block">
-                            <a href="{{action('PostController@show', $post->id)}}" class="button">{{ $post->views }} {{str_plural('view', $post->views)}}</a>
+                            <a href="{{action('PostController@show', $post->id)}}" class="button">{{ $post->views }} {{ str_plural('view', $post->views) }}</a>
                             <a href="{{action('PostController@show', $post->id)}}" class="button @if($post->isAccepted()) is-success @endif">
                                 @unless($post->isAccepted())
-                                    {{ $post->getStats() }}  {{str_plural('answer', $post->getStats()) }}
+                                    {{ $post->getStats() }}  {{ str_plural('answer', $post->getStats()) }}
                                 @else
                                     accepted
                                 @endunless
                             </a>
-                            <a href="{{action('PostController@show', $post->id)}}" class="button">{{$post->votes}}  {{str_plural('vote', $post->votes)}}</a>
+                            <a href="{{action('PostController@show', $post->id)}}" class="button">{{ $post->votes }}  {{ str_plural('vote', $post->votes) }}</a>
                         </div>
                     </figure>
                     <div class="media-content">
                         <div class="content">
                             <p>
-                                <strong>{{$post->author->name}}</strong>
+                                <strong>{{ $post->author->name }}</strong>
                                 <br>
-                                <a href="{{action('PostController@show', $post->id)}}">{{$post->title}}</a>
+                                <a href="{{action('PostController@show', $post->id)}}">{{ $post->title }}</a>
                             </p>
 
                         </div>
