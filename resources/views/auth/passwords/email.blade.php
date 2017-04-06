feaee65e60dbb505fe986dbd06bfc91f11c6c7b9
Sven Luijten <svenluijten1995@gmail.com>
2017-04-06 23:06:10 +0200

    Make password reset form use Bulma

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,45 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="columns">
+    <div class="column is-one-third is-offset-one-third panel">
+        <p class="panel-heading">
+            Reset Password
+        </p>
+        <div class="panel-block">
+            @if(session('status'))
+                <div class="notification">
+                    <button class="delete"></button>
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <form role="form" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
+
+                <div class="field">
+                    <label for="email" class="label">E-Mail Address</label>
+
+                    <p class="control">
+                        <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    </p>
+
+                    @if($errors->has('email'))
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @endif
+                </div>
+
+                <div class="field">
+                    <p class="control">
+                        <button class="button is-primary" type="submit">Submit</button>
+                    </p>
+                </div>
+            </form>
         </div>
     </div>
 </div>
