7d3e032ceee4557d6cf02d544ca0b57c86faf0cf
Sven Luijten <svenluijten1995@gmail.com>
2017-04-06 19:52:36 +0200

    Fix code style in database/ folder

--- database/seeds/UsersAndHouseRolesTableSeeder.php
+++ database/seeds/UsersAndHouseRolesTableSeeder.php
@@ -5,24 +5,27 @@ use Faker\Factory as Faker;
 use App\HouseRole;
 use App\User;
 
-class UsersAndHouseRolesTableSeeder extends Seeder {
+class UsersAndHouseRolesTableSeeder extends Seeder
+{
+    /**
+     * Run the seeder.
+     */
     public function run()
     {
         $faker = Faker::create();
 
-        foreach(range(1, 50) as $index)
-        {
+        foreach(range(1, 50) as $index) {
             $user = User::create([
                 'name' => $faker->firstName . ' (the ' . $index . 'st) ' . $faker->lastName,
                 'email' => $faker->email,
-                'password' => 'secret'
+                'password' => 'secret',
             ]);
 
-            $houseRole = HouseRole::create([
+            HouseRole::create([
                 'user_id' => $user->id,
                 'house_id' => rand(1, 4),
                 'role_title' => $faker->title,
-                'role_level' => rand(0, 1) * 100
+                'role_level' => rand(0, 1) * 100,
             ]);
         }
     }
