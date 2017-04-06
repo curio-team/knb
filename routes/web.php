924da3beb80aab1c512f9df84100b052790e27e0
Sven Luijten <svenluijten1995@gmail.com>
2017-04-06 19:29:28 +0200

    Clean up routes

--- routes/web.php
+++ routes/web.php
@@ -17,47 +17,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function() {
 
     Route::get('/home', 'HomeController@index');
-    Route::resource('house', 'HouseController', [
-        'names' => [
-            'index' => 'house.index',
-            'show' => 'house.show',
-            'create' => 'house.create',
-            'store' => 'house.store',
-            'edit' => 'house.edit',
-            'update' => 'house.update',
-            'destroy' => 'house.destroy',
-        ]
-    ]);
+
+    Route::resource('house', 'HouseController');
+    Route::resource('post', 'PostController');
+    Route::resource('comment', 'CommentController');
 
     Route::get('post/{id}/answer', 'PostController@answer')->name('answer');
-
-    Route::resource('post', 'PostController', [
-        'names' => [
-            'index' => 'post.index',
-            'show' => 'post.show',
-            'create' => 'post.create',
-            'store' => 'post.store',
-            'edit' => 'post.edit',
-            'update' => 'post.update',
-            'destroy' => 'post.destroy',
-        ]
-    ]);
-
-    Route::resource('comment', 'CommentController', [
-        'names' => [
-            'index' => 'comment.index',
-            'show' => 'comment.show',
-            'create' => 'comment.create',
-            'store' => 'comment.store',
-            'edit' => 'comment.edit',
-            'update' => 'comment.update',
-            'destroy' => 'comment.destroy',
-        ]
-    ]);
-
 });
-
-
