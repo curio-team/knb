fd2974b2b39bceecf81321fc8311c0121d1afc9a
Radiuscollege <feddevangils@gmail.com>
2017-05-19 15:23:33 +0200

    fix upvote

--- resources/views/partials/minis/_vote-group.blade.php
+++ resources/views/partials/minis/_vote-group.blade.php
@@ -1,8 +1,8 @@
 <div class="vote-group">
     @unless($post->isYours() || $post->userHasVoted())
         <span class="fa fa-arrow-up fa-2x" onclick="event.preventDefault();
-            $(this).closest('.button-group').find('.vote-form .vote-type').val('up');
-            $(this).closest('.button-group').find('.vote-form').submit();" >
+            $(this).closest('.vote-group').find('.vote-form .vote-type').val('up');
+            $(this).closest('.vote-group').find('.vote-form').submit();" >
         </span>
     @endunless
 
@@ -11,9 +11,10 @@
     @if($post->userHasVoted())
         voted
     @endif
+        <form class="vote-form" data-vote="null" action="{{ action('PostController@vote', $post) }}" method="POST" style="display: none;">
+            <input type="hidden" name="vote" class="vote-type" value="">
+            {{ csrf_field() }}
+        </form>
 </div>
 
-<form class="vote-form" data-vote="null" action="{{ action('PostController@vote', $post) }}" method="POST" style="display: none;">
-    <input type="hidden" name="vote" class="vote-type" value="">
-    {{ csrf_field() }}
-</form>
+
