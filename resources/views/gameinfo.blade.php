980d2ad3156dcb6ea0505ca8a27637112739832e
Radiuscollege <feddevangils@gmail.com>
2017-06-14 15:34:32 +0200

    add csv import + game info

---
+++ resources/views/gameinfo.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="content container main-content">
+
+        <h1 class="is-1">Earnings</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque, esse ex excepturi facilis hic inventore iure laborum maiores maxime nihil nulla optio quisquam quo suscipit tempore vel voluptatum. Laboriosam.</p>
+
+        <h2 class="is-2">Points</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur distinctio eius necessitatibus nemo obcaecati reprehenderit suscipit ullam velit! Cumque expedita id illo inventore laborum nemo nihil quis repellendus tempora voluptate!</p>
+
+
+        <h2 class="is-2">Badges</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi, assumenda, aut autem commodi consequuntur dolorem doloremque ea, eligendi excepturi facere fuga impedit minima officia pariatur rem repellat sequi totam?</p>
+
+    </div>
+
+@endsection
