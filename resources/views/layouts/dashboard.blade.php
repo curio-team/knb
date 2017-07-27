0842fcd46d760dd199cb364228d68f8456e16cdc
Radiuscollege <feddevangils@gmail.com>
2017-07-27 08:32:43 +0200

    add gsuite

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="FSCUKOCngFG_EfSGlIBJSMBeKuLfP-zkde0wsk0ZeqA" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
