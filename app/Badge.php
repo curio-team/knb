6cd41bb8a82fd76a9b4216c3e636034cd6267b95
Radiuscollege <feddevangils@gmail.com>
2017-10-31 14:29:38 +0100

    fix;

--- app/Badge.php
+++ app/Badge.php
@@ -11,7 +11,7 @@ class Badge extends Model
     }
 
     public function getImage() {
-        return public_path("img\badges\$this->img_path");
+        return public_path("img/badges/$this->img_path");
 
     }
 
