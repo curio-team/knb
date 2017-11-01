21f70bf350ea7ed2ae56cb99a0d337dd95997137
Radiuscollege <feddevangils@gmail.com>
2017-11-01 10:42:22 +0100

    change message;

--- resources/views/emails/badge/request.blade.php
+++ resources/views/emails/badge/request.blade.php
@@ -1,12 +1,9 @@
 @component('mail::message')
 # A new badge request is done.
 
-{{$user->name}} requested the badge {{ $badge->name }}
+{{$user->name}} requested the badge {{ $badge->title }}
 Please look into it.
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
 
 Thanks,<br>
 {{ config('app.name') }}
