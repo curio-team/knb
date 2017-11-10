c5a863f210282d7b586e4f1ea5e5ac73e7dc368e
Ricardo van Bree <ricardovbree@gmail.com>
2017-11-10 20:20:12 +0100

    style met een kleine beetje responsief voor mobiel

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -5,7 +5,7 @@
         <div class="content columns">
             <div class="column is-three-quarters">
               <div class="news-show">
-                  <h1 class="news-title">{{$newsItem->title}}</h1>
+                  <h1>{{$newsItem->title}}</h1>
                   <p>{!! nl2br($newsItem->content)  !!}</p>
               </div>
             </div>
