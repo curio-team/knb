7a64d97427b263ff4e83770ff2d43d25b34693c7
Radiuscollege <feddevangils@gmail.com>
2017-11-30 15:44:21 +0100

    change statistics page;

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -6,13 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css">
-    <title>Document</title>
+
+    <title>Statistics</title>
     <style>
         html {
             background: linear-gradient(to right, #71B280, #134E5E)
         }
     </style>
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0s5ljyxk73uoispvgp9i23onutacx4fx2agrlzv6iglqw4zv"></script>
+
+ <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0s5ljyxk73uoispvgp9i23onutacx4fx2agrlzv6iglqw4zv"></script>
     <script src="{{ asset('js/prism.js') }}"></script>
 
     <script>
@@ -29,6 +31,7 @@
     <div id="app">
         <statistics></statistics>
     </div>
+
 
 
         <script src="{{ asset('js/app.js') }}"></script>
